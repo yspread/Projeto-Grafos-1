7a2522567251bd0a7fbd5862ae12cfcd10d17cf9
Rafael Pavon Diesner <rafaeldiesner@usp.br>
2026-03-17 19:33:13 -0300

    Add files via upload

---
+++ graph.c
@@ -0,0 +1,167 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+#include "graph.h"
+
+typedef struct grafo_
+{
+    int nVertices;
+    int **matrizAdjacencia;
+}GRAFO;
+
+GRAFO* criargrafo(int n)
+{
+    GRAFO *grafo = (GRAFO *)malloc(sizeof(GRAFO));
+    if (grafo != NULL)
+    {
+        grafo->nVertices = n;
+        grafo->matrizAdjacencia = (int **)malloc(n * sizeof(int *)); //alocando a matriz de adjacencia dinamicamente
+        for (int i = 0; i < n; i++)
+        {
+            grafo->matrizAdjacencia[i] = (int)malloc(n * sizeof(int));
+        }
+        for (int i = 0; i < n; i++) //loop para inicializar a matriz como -1 
+        {
+            for (int j = 0; j < n; j++)
+            {
+                grafo->matrizAdjacencia[i][j] = -1; //-1 indica a ausência de aresta entre os vértices i e j
+            }
+        }
+    }
+    return grafo;
+}
+
+void addaresta(GRAFO *grafo, int v1, int v2, int peso)  //!!!!!!!!!PRECISO SABER SE É DIRECIONADO OU NÃO!!!!
+{
+    if (grafo != NULL)
+    {
+        grafo->matrizAdjacencia[v1-1][v2-1] = peso; //escrevo o peso na posição correta da matriz de adjacência;
+        grafo->matrizAdjacencia[v2-1][v1-1] = peso; //adiciono no simétrico também, considerando arestas bidirecionais 
+    }
+}
+
+int existearesta(GRAFO *grafo, int v1, int v2)
+{
+    if (grafo != NULL)
+    {
+        if (grafo->matrizAdjacencia[v1-1][v2-1] != -1) //-1 na matriz indica que a aresta não existe
+        {
+            return 1; //a aresta entre v1 e v2 existe
+        }
+        else
+        {
+            return 0;//a aresta entre v1 e v2 não existe
+        }
+    }
+}
+void vizinhos (GRAFO *grafo, int v)
+{
+    if (grafo != NULL)
+    {
+        for (int i = 0; i < grafo->nVertices; i++) //percorro a linha da matriz correspondente ao vertice v
+        {
+            if (grafo->matrizAdjacencia[v-1][i] != -1) //caso o valor da matriz seja diferente de -1, existirá uma aresta entre o vértice v e o vértice correspondente a coluna
+            {
+                printf("%d ", i); //imprimo o número do vértice (também é o número da coluna) que divide aresta com v
+            }
+        }
+    }
+}
+
+int apagaaresta (GRAFO *grafo, int v1, int v2)
+{
+    if (grafo != NULL)
+    {
+        if (grafo->matrizAdjacencia[v1-1][v2-1] == -1) //caso a aresta não exista, printa e retorna -1
+        {
+            printf("-1");
+            return -1;
+        }
+        else //caso a aresta existe (seja diferente de -1), colocamos -1 nos espaços equivalentes a aresta a ser apagada
+        {
+            grafo->matrizAdjacencia[v1-1][v2-1] = -1;
+            grafo->matrizAdjacencia[v2-1][v1-1] = -1;
+            return 1;
+        }
+    }
+}   
+void printinfo (GRAFO *grafo)
+{
+    if (grafo != NULL)
+    {
+        printf("V = [");
+        for(int i = 1; i <= grafo->nVertices; i++)
+        {   
+            if (i != grafo->nVertices)
+            {
+                printf("%d, ", i);
+            }
+            else{
+                printf("%d]\n", i);
+            }
+        }
+        printf ("E = [");
+        for (int i = 0; i < grafo->nVertices; i++)
+        {
+            for (int j = i; j < grafo->nVertices; j++)
+            {
+                if (grafo->matrizAdjacencia[i][j] != -1)
+                {
+                    printf("(%d, %d),  ", (i+1), (j+1));
+                }
+            }
+        }
+        printf("\b\b]");
+    }
+}
+
+void apagargrafo(GRAFO **grafo)
+{
+    if (*grafo != NULL)
+    {
+        //libero toda a memória alocada para a matriz de adjacencia
+        for (int i = 0; i < (*grafo)->nVertices; i++)
+        {
+            free((*grafo)->matrizAdjacencia[i]);
+            (*grafo)->matrizAdjacencia[i] = NULL;
+        }
+        free ((*grafo)->matrizAdjacencia);
+        (*grafo)->matrizAdjacencia = NULL;
+    }
+    free(*grafo); //libero a memória alocada para o grafo
+    *grafo = NULL;
+}
+
+int maisvizinhos(GRAFO *grafo)
+{
+    if (grafo != NULL)
+    {
+        int vertice, contador = 0, maior = 0; //vertice armazenará o vértice com mais vizinhos, contador conta quantos vizinhos cada vértice tem, maior guarda o n de vizinhos do vertice que mais tem ate então
+        for (int i = 0; i < grafo->nVertices; i++) //percorro pela matriz
+        {
+            for (int j = 0; j < grafo->nVertices; j++)
+            {
+                if (grafo->matrizAdjacencia[i][j] != -1) //verifico quantas arestas cada vértice tem, vertice por vertice
+                {
+                    contador++;
+                }
+            }
+            if (contador > maior) //se o contador for maior que o maior numero de vizinhos ate entao, esse valor será o novo maior numero de vizinhos
+            {
+                vertice = i; //o vertice atual é o novo vértice com mais vizinhos
+                maior = contador;
+            }
+            contador = 0; //o contador reinicia para o próximo vertice
+        }
+        return (vertice+1); // a posição do vértice na matriz é sempre 1 a menos do que o numero do vértice, pois a matriz começa no 0 e não no 1
+    }
+    return 0;
+}
+
+int **getmatrizadj(GRAFO *grafo)
+{
+    if (grafo != NULL)
+    {
+        return (grafo->matrizAdjacencia); //função que retorna a matriz de adjacencia
+    }
+}
---
+++ main.c
@@ -0,0 +1,13 @@
+// código da sorte
+// que teus bugs nunca sejam longos demais
+// nem tua memória acabe na hora de desespero 
+
+#include "graph.h"
+#include <stdio.h>
+#include <stdbool.h>
+#include <stdlib.h>
+
+int main()
+{
+    
+}
