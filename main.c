1945bd00e9099f90081c6c49faa9422f3331d38e
Rafael Pavon Diesner <rafaeldiesner@usp.br>
2026-03-26 10:30:51 -0300

    add main
    essa main foi passada pelo monitor, talvez precise de ajustes

--- main.c
+++ main.c
@@ -1,13 +1,47 @@
-// código da sorte
-// que teus bugs nunca sejam longos demais
-// nem tua memória acabe na hora de desespero 
-
-#include "graph.h"
 #include <stdio.h>
 #include <stdbool.h>
-#include <stdlib.h>
+#include "graph.h"
 
 int main()
 {
-    
+    int option;
+    int N, x, y, w;
+    int res;
+    bool printstatus = 1;
+    GRAFO *G;
+    scanf("%d", &option);
+    while(option != -1)
+    {
+        switch(option)
+        {
+        case 0:
+            scanf("%d", &N);
+            G = criargrafo(N);
+            break;
+        case 1:
+            scanf("%d %d %d", &x, &y, &w);
+            addaresta(G, x, y, w);                
+            break;
+        case 2:
+            scanf("%d %d", &x, &y);
+            res = existearesta(G, x, y);
+            printstatus = 0;
+            break;
+        default:
+            printf("unrecognized option %d!\n", option);
+        }
+        scanf("%d\n", &option);
+    }
+    if (option == -1)
+    {
+        if(printstatus)
+        {
+            printinfo(G);
+        }
+        else{
+            printf("%d\n", res);
+        }
+    }
+    apagargrafo(&G);
+    return 0;
 }
